3e909531b42c4a4c78db7d2256cc3809b35d3c12
Harry <76102114+HarryPilgrim@users.noreply.github.com>
2025-09-19 14:20:53 +0100

    qura website

---
+++ src/_data/client.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: "Qura app",
+    email: "hello@quraapp.com",
+    phoneForTel: "+447356271234",
+    phoneFormatted: "+44 7356 271 234",
+    address: {
+        lineOne: "Neon Flow",
+        lineTwo: "Headquarters",
+        city: "Darlington",
+        state: "Durham",
+        zip: "DL1",
+        country: "UK",
+        mapLink: "https://maps.app.goo.gl/3DJ99n3JC93p5a5m7",
+    },
+    socials: {
+        facebook: "https://www.facebook.com/",
+        instagram: "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    domain: "https://www.quraapp.com",
+    // Passing the isProduction variable for use in HTML templates
+    isProduction: process.env.ELEVENTY_ENV === "PROD",
+};
